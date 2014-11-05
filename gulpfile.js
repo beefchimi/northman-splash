86506fcc8b0e674e68fa0a8a19652ff09d842415
beefchimi <curtdulmage@gmail.com>
2014-11-05 12:07:34 -0500

    finished basic styling

--- gulpfile.js
+++ gulpfile.js
@@ -171,7 +171,7 @@ gulp.task('extras', function() {
 });
 
 
-/*
+
 // Use rsync to deploy to server (no need to exclude files since everything comes from 'build' folder)
 gulp.task('deploy', function() {
 
@@ -188,7 +188,7 @@ gulp.task('deploy', function() {
 		}));
 
 });
-*/
+
 
 
 // Watch over specified files and run corresponding tasks...
